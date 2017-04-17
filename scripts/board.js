d9f5e16bb04c5da7400173a7e65ce9350ed1dd1d
Simen Hiken Galtestad <simen.galtestad@gmail.com>
2017-04-17 17:25:49 +0200

    removed unused files

--- scripts/board.js
+++
@@ -1,8 +0,0 @@
-function Board(width, height) {
-    this.width = width;
-    this.height = height;
-    this.show = function() {
-        var canvas = document.getElementById("board");
-        var context = canvas.getContext("2d");
-    };
-}
